f118f8b2d57d93a97486a0ec289fce32bec87273
awesome2292 <awesome2292@users.noreply.github.com>
2019-10-15 19:53:08 -0400

    Character Horizontal Movement Implemented (w/ Animations)

--- CART 415 Game/Assets/Scripts/CharacterController.cs
+++ CART 415 Game/Assets/Scripts/CharacterController.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public Animator animator;
+    public float speed;
 
     // Update is called once per frame
     void Update()
     {
-        
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal * speed * Time.deltaTime;
+
     }
 }
