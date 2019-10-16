f965415d8e50fc626a3e6d45c5054aeca4ee3479
awesome2292 <awesome2292@users.noreply.github.com>
2019-10-16 17:39:59 -0400

    Added Collision and Climbing effects

---
+++ CART 415 Game/Assets/Scripts/Ladders.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladders : MonoBehaviour
+{
+    public float ClimbingSpeed;
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.tag=="Player" && Input.GetKey("up"))
+        {
+            Debug.Log("player collides with ladder");
+            other.GetComponent<Rigidbody2D>().velocity = new Vector3(0.0f, ClimbingSpeed, 0.0f);
+        }
+
+        else if(other.tag=="Player" && Input.GetKey("down"))
+        {
+            Debug.Log("player collides with ladder");
+            other.GetComponent<Rigidbody2D>().velocity = new Vector3(0.0f, ClimbingSpeed, 0.0f);
+        }
+    }
+
+}
---
+++ CART 415 Game/Assets/Scripts/SpriteController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Animator animator;
+    public float WalkingSpeed;
+    public float RunningSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal * WalkingSpeed * Time.deltaTime;
+        animator.SetBool("ShiftPressed", false);
+
+        if (Input.GetKey("left shift"))
+        {
+            transform.position = transform.position + horizontal * RunningSpeed * Time.deltaTime;
+            animator.SetBool("ShiftPressed", true);
+        }
+
+        //if (Input.GetKey("i"))
+        //{
+        //    Debug.Log("The Character is interacting with the NPC");
+        //}
+
+    }
+}
